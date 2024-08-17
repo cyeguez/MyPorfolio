45b17fdf057b727a6f003be10dfac9087e3af841
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-08-17 13:50:37 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+const modeDark = document.getElementById("btn-mode-dark");
+const iconMenu = document.querySelector(".icon-menu");
+const iconClose = document.querySelector(".icon-close");
+const nav = document.querySelector(".nav");
+const fullName = document.getElementById("full-name");
+const mail = document.getElementById("mail");
+const errorNotification = document.querySelector(".error-notification");
+const textarea = document.querySelector(".form__textarea");
+const phone = document.querySelector(".form__phone");
+
+console.log(textarea);
+
+iconMenu.addEventListener("click", () => {
+  iconMenu.classList.add("off");
+  iconClose.classList.add("on");
+  nav.classList.add("open-menu");
+});
+
+iconClose.addEventListener("click", () => {
+  iconMenu.classList.remove("off");
+  iconClose.classList.remove("on");
+  nav.classList.remove("open-menu");
+});
+
+// Activación del modo Dark
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.documentElement;
+
+  if (root.getAttribute("data-theme") === "dark") {
+    modeDark.checked = true;
+   
+  }
+
+  modeDark.addEventListener("change", () => {
+    toggleModoDark();
+
+  });
+
+  function toggleModoDark() {
+    const setTheme=  modeDark.checked ? 'dark' : 'light';
+
+
+    root.setAttribute('data-theme', setTheme);
+    localStorage.setItem('theme', setTheme);
+  }
+
+
+});
+// Funcion encargada de validar los Input
+
+fullName.addEventListener("change", (e) => {
+  validationInput(e);
+});
+
+mail.addEventListener("change", (e) => {
+  validationInput(e);
+});
+phone.addEventListener("change", (e) => {
+  validationInput(e);
+});
+textarea.addEventListener("change", (e) => {
+  validationInput(e);
+});
+
+function toogleError(nameInput) {
+  let containerFather = nameInput.parentNode;
+
+  if (nameInput.checkValidity()) {
+    containerFather.classList.remove("error");
+  } else {
+    containerFather.classList.add("error");
+  }
+}
+
+function validationInput(e) {
+  let typeInput = e.target;
+
+  if (typeInput.type === "text") {
+    toogleError(typeInput);
+  } else if (typeInput.type === "email") {
+    toogleError(typeInput);
+  } else if (typeInput.type === "tel") {
+    toogleError(typeInput);
+  } else {
+    toogleError(textarea);
+  }
+}
